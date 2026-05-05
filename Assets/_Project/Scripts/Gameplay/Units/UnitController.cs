4aa671454fafe30a5fe7bf8cdfb636256d6adac7
VinhQui <phamqui620@gmail.com>
2026-05-05 18:49:31 +0700

    feat: implement basic unit movement with click input
    - Added UnitMovement component
    - Implemented MoveTo logic using Vector3.MoveTowards
    - Added UnitController to handle mouse input
    - Right click to move unit to target position

---
+++ Assets/_Project/Scripts/Gameplay/Units/UnitController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour     // nhập input / điều khiển
+{
+    private UnitMovement movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<UnitMovement>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
+            movement.MoveTo(mousePos);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Gameplay/Units/UnitMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour   // xử lý di chuyển
+{
+    [SerializeField] float moveSpeed = 3f;
+    private Vector3 _targetPos;
+    private bool isMoving = false;
+    
+    public void MoveTo(Vector3 pos)
+    {
+        _targetPos = pos;
+        isMoving = true;
+    }
+
+    private void Update()
+    {
+        if (!isMoving) return;
+
+        transform.position = Vector3.MoveTowards(transform.position, _targetPos, moveSpeed * Time.deltaTime);
+
+        float distance = Vector3.Distance(transform.position, _targetPos);
+
+        if (distance < 0.05f)
+        {
+            isMoving = false;
+        }
+    }
+}
