c11d52c27c7217495ecce7c88cfae8292d490b4f
VinhQui <phamqui620@gmail.com>
2026-05-08 16:57:42 +0700

    feat: implement pawn finite state machine foundation
    - Added PawnController to manage pawn states
    - Created abstract PawnState base class
    - Implemented state transition flow with Enter/Update/Exit
    - Prepared FSM architecture for future pawn behaviors

---
+++ Assets/_Project/Scripts/Gameplay/Pawn/PawnController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnController : MonoBehaviour     // quản lý state hiện tại, chuyển state,..
+{
+    private PawnState currentState;
+    private void Start()
+    {
+        Debug.Log("Pawn FSM Ready");
+    }
+    private void Update()
+    {
+        // nếu currentState khác null -> gọi Update()
+        currentState?.Update();
+        // nếu viết currentState.Update() thì nếu currentState = null -> Crash.
+    }
+    public void ChangeState(PawnState newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState?.Enter();
+    }
+}
---
+++ Assets/_Project/Scripts/Gameplay/Pawn/PawnState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// "abtract" để ko dùng trực tiếp, chỉ để kế thừa
+public abstract class PawnState      // mọi state kế thừa từ đây: IdleState, GatherState, etc.
+{
+    protected PawnController pawn;
+    public PawnState(PawnController pawn)
+    {
+        this.pawn = pawn;
+    }
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+}
